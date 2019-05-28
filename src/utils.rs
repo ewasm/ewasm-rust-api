92ddc77637a6762e34e0da60fdf8fa0d3e0851fe
Alex Beregszaszi <alex@rtfs.hu>
2019-05-28 15:31:53 +0100

    Split out some utilities
    And add tests for them.

---
+++ src/utils.rs
@@ -0,0 +1,19 @@
+#[cfg(feature = "std")]
+pub fn unsafe_alloc_buffer(len: usize) -> Vec<u8> {
+    let mut ret: Vec<u8> = Vec::with_capacity(len);
+    unsafe {
+        ret.set_len(len);
+    }
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn smoke() {
+        let ret = unsafe_alloc_buffer(42);
+        assert_eq!(ret.len(), 42);
+    }
+}
