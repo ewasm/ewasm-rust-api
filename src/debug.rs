e5669bd1984a25cc9b7ab8792c01822652011f97
Alex Beregszaszi <alex@rtfs.hu>
2019-05-28 19:40:45 +0100

    Fix compilation issues in the debug API

--- src/debug.rs
+++ src/debug.rs
@@ -1,6 +1,8 @@
 //! The native debug interface exposed to the ewasm contract. These functions are for testing
 //! purposes only. On a live VM, any bytecode trying to import these symbols will be rejected.
 
+use crate::types::*;
+
 mod native {
     extern "C" {
         pub fn debug_print32(value: u32);
@@ -16,16 +18,16 @@ pub fn print32(value: u32) {
     unsafe { native::debug_print32(value) }
 }
 
-pub fn print64(value: u32) {
+pub fn print64(value: u64) {
     unsafe { native::debug_print64(value) }
 }
 
 pub fn printMem(slice: &[u8]) {
-    unsafe { native::debug_printMem(slice.bytes.as_ptr() as *const u32, slice.len()) }
+    unsafe { native::debug_printMem(slice.as_ptr() as *const u32, slice.len() as u32) }
 }
 
 pub fn printMemHex(slice: &[u8]) {
-    unsafe { native::debug_printMem(slice.bytes.as_ptr() as *const u32, slice.len()) }
+    unsafe { native::debug_printMem(slice.as_ptr() as *const u32, slice.len() as u32) }
 }
 
 pub fn printStorage(key: &StorageKey) {
