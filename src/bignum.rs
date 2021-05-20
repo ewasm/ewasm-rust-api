fbd49ae75dc47fb02b35f9006f510ca90a0187de
Antonio Yang <yanganto@gmail.com>
2021-05-20 15:38:35 +0800

    Use wasm_import_module and remove function name prefix

--- src/bignum.rs
+++ src/bignum.rs
@@ -4,9 +4,10 @@ use crate::types::Uint256;
 /// The low-level interface to the system library. Use the wrapper functions unless you know what
 /// you're doing.
 pub mod native {
+    #[link(wasm_import_module = "bignum")]
     extern "C" {
-        pub fn bignum_mul256(a: *const u32, b: *const u32, ret: *const u32);
-        pub fn bignum_umulmod256(a: *const u32, b: *const u32, modulo: *const u32, ret: *const u32);
+        pub fn mul256(a: *const u32, b: *const u32, ret: *const u32);
+        pub fn umulmod256(a: *const u32, b: *const u32, modulo: *const u32, ret: *const u32);
     }
 }
 
@@ -15,7 +16,7 @@ pub fn mul256(a: &Uint256, b: &Uint256) -> Uint256 {
     let mut ret = Uint256::default();
 
     unsafe {
-        native::bignum_mul256(
+        native::mul256(
             a.bytes.as_ptr() as *const u32,
             b.bytes.as_ptr() as *const u32,
             ret.bytes.as_mut_ptr() as *const u32,
@@ -30,7 +31,7 @@ pub fn umulmod256(a: &Uint256, b: &Uint256, modulo: &Uint256) -> Uint256 {
     let mut ret = Uint256::default();
 
     unsafe {
-        native::bignum_umulmod256(
+        native::umulmod256(
             a.bytes.as_ptr() as *const u32,
             b.bytes.as_ptr() as *const u32,
             modulo.bytes.as_ptr() as *const u32,
--- src/debug.rs
+++ src/debug.rs
@@ -5,47 +5,48 @@ use crate::types::StorageKey;
 
 /// The native interface for debugging functions.
 mod native {
+    #[link(wasm_import_module = "debug")]
     extern "C" {
-        pub fn debug_print32(value: u32);
-        pub fn debug_print64(value: u64);
-        pub fn debug_printMem(offset: *const u32, len: u32);
-        pub fn debug_printMemHex(offset: *const u32, len: u32);
-        pub fn debug_printStorage(pathOffset: *const u32);
-        pub fn debug_printStorageHex(pathOffset: *const u32);
+        pub fn print32(value: u32);
+        pub fn print64(value: u64);
+        pub fn printMem(offset: *const u32, len: u32);
+        pub fn printMemHex(offset: *const u32, len: u32);
+        pub fn printStorage(pathOffset: *const u32);
+        pub fn printStorageHex(pathOffset: *const u32);
     }
 }
 
 /// Prints a string.
 pub fn log(msg: &str) {
-    unsafe { native::debug_printMem(msg.as_ptr() as *const u32, msg.len() as u32) }
+    unsafe { native::printMem(msg.as_ptr() as *const u32, msg.len() as u32) }
 }
 
 /// Prints an unsigned 32-bit int.
 pub fn print32(value: u32) {
-    unsafe { native::debug_print32(value) }
+    unsafe { native::print32(value) }
 }
 
 /// Prints an unsigned 64-bit int.
 pub fn print64(value: u64) {
-    unsafe { native::debug_print64(value) }
+    unsafe { native::print64(value) }
 }
 
 /// Prints the contents of a slice.
 pub fn print_mem(slice: &[u8]) {
-    unsafe { native::debug_printMem(slice.as_ptr() as *const u32, slice.len() as u32) }
+    unsafe { native::printMem(slice.as_ptr() as *const u32, slice.len() as u32) }
 }
 
 /// Prints the contents of a slice in hexadecimal format.
 pub fn print_mem_hex(slice: &[u8]) {
-    unsafe { native::debug_printMemHex(slice.as_ptr() as *const u32, slice.len() as u32) }
+    unsafe { native::printMemHex(slice.as_ptr() as *const u32, slice.len() as u32) }
 }
 
 /// Prints the value of a storage key.
 pub fn print_storage(key: &StorageKey) {
-    unsafe { native::debug_printStorage(key.bytes.as_ptr() as *const u32) }
+    unsafe { native::printStorage(key.bytes.as_ptr() as *const u32) }
 }
 
 /// Prints the value of a storage key in hexadecimal format.
 pub fn print_storage_hex(key: &StorageKey) {
-    unsafe { native::debug_printStorageHex(key.bytes.as_ptr() as *const u32) }
+    unsafe { native::printStorageHex(key.bytes.as_ptr() as *const u32) }
 }
